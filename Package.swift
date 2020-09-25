d7813d1e87e41270cc4912fd272dcf522e36bc61
Stephan Boner <stephan.boner@bluewin.ch>
2020-09-25 18:21:19 +0200

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,26 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "RangeSeekSlider",
+            dependencies: [],
+            path: "Sources/"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
